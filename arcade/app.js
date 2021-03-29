4c0c56755907a30dbb6de19f29888af0c7879536
power-david <78715567+power-david@users.noreply.github.com>
2021-03-28 21:59:24 -0700

    Add files via upload
    arcade_final

--- arcade/app.js
+++ arcade/app.js
@@ -1,33 +1,24 @@
-// state
 const initialState = {
     players: ['x', 'o'],
-
     currentPlayer: null,
-
     winner: null,
-
-    // multi-dimensional array
     board: [
       ['', '', ''],
       ['', '', ''],
       ['', '', '']
-    ]
+    ],
   }
 
 let gameState
 
-  //in a multi-dimensional array, to get the 1st square on the 2nd row
-  //intitialState.board[1][0]
-
-  //in buildInitialState, current player is whomever is 
-  //in the 0 position of gameState.players, swap after each play
-  
 function buildInitialState() {
+    const footer = $('footer')
+    footer.empty()
+    console.log()
     gameState = JSON.parse(JSON.stringify(initialState))
     renderState()
 }
 
-// render
 function renderState() {
     const app = $('#app')
     app.empty()
@@ -42,40 +33,22 @@ function renderState() {
     })
 }
 
-// maybe a dozen or so helper functions for tiny pieces of the interface
-
-// listeners
 function onSquareClick() {
-if ($(this).text().trim() === "" && gameState.winner === null) {
-    const row = $(this).data("row")
-    const square = $(this).data("square")
+    if ($(this).text().trim() === "" && gameState.winner === null) {
+        const row = $(this).data("row")
+        const square = $(this).data("square")
 
     gameState.board[row][square] = selectPlayer()
-    // gameState.board[row][square] = 'o'
-    //is this a winning space?
    
     renderState()
     checkForWinner()
 }
 
     if (gameState.winner) {
-        const winnerElement = $(`<div>${gameState.winner} is the winner!</div>`)
+        const winnerElement = $(`<div class="game-message">${gameState.winner} is the winner!</div>`)
         $('footer').append(winnerElement)
-
-
-        // alert(`${gameState.winner} is the winner!`)
-        // resetGame()
-    }
-
-    
- // update state, maybe with another dozen or so helper functions...
-
-//   renderState() // show the user the new state
+    } 
 }
-
-//functions to create
-
-//1. Take turns placing marks in empty spaces (see onSquareClick)
 
 function selectPlayer() {
     if (gameState.currentPlayer == 'x' ) {
@@ -84,45 +57,32 @@ function selectPlayer() {
         gameState.currentPlayer = 'x'
     }
     return gameState.currentPlayer
-}
-
-//3. Notify when a move results in a win or draw
-
-//4. Disable user clicking when a game is over, allow user to reset game when game is over
+} 
 
 $('#reset').click(function() {
     resetGame()
 })
 
-//5. Reset game without resetting browser
-
 function resetGame() {
-    // renderState()
     buildInitialState()
 }
-
-//6. Check for a winner after each play
 
 function checkForWinner() {
     let board = gameState.board
     if (
-        (board[0][0] == gameState.currentPlayer && board[0][1] == gameState.currentPlayer && board[0][2] == gameState.currentPlayer)
-        // (board[1][0] && board[1][1] && board[1][2]) ||
-        // (board[2][0] && board[2][1] && board[2][2]) ||
-        // (board[0][0] && board[1][0] && board[2][0]) ||
-        // (board[0][1] && board[1][1] && board[2][1]) ||
-        // (board[0][2] && board[1][2] && board[2][2]) ||
-        // (board[0][0] && board[1][1] && board[2][2]) ||
-        // (board[0][2] && board[1][1] && board[2][0]) 
+        (board[0][0] == gameState.currentPlayer && board[0][1] == gameState.currentPlayer && board[0][2] == gameState.currentPlayer) ||
+        (board[1][0] == gameState.currentPlayer && board[1][1] == gameState.currentPlayer && board[1][2] == gameState.currentPlayer) ||
+        (board[2][0] == gameState.currentPlayer && board[2][1] == gameState.currentPlayer && board[2][2] == gameState.currentPlayer) ||
+        (board[0][0] == gameState.currentPlayer && board[1][0] == gameState.currentPlayer && board[2][0] == gameState.currentPlayer) ||
+        (board[0][1] == gameState.currentPlayer && board[1][1] == gameState.currentPlayer && board[2][1] == gameState.currentPlayer) ||
+        (board[0][2] == gameState.currentPlayer && board[1][2] == gameState.currentPlayer && board[2][2] == gameState.currentPlayer) ||
+        (board[0][0] == gameState.currentPlayer && board[1][1] == gameState.currentPlayer && board[2][2] == gameState.currentPlayer) ||
+        (board[0][2] == gameState.currentPlayer && board[1][1] == gameState.currentPlayer && board[2][0] == gameState.currentPlayer) 
     ){
-        console.log("Current player is the winner", gameState.currentPlayer)
         gameState.winner = gameState.currentPlayer
-
-        //notify which player has won, reset board, and change button to play again
-    }
+    } 
 }
 
 buildInitialState()
-
 renderState()
-$('#app').on('click', '.square', onSquareClick); // etc
+$('#app').on('click', '.square', onSquareClick)
